b8a315d74b132a20e2cb419c0a3d62d570c85f84
dhiman10 <dhimandas10.sec@gmail.com>
2025-08-07 16:22:53 +0200

    Initial Set Up

---
+++ AIChatMate/AIChatMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatMateApp.swift
+//  AIChatMate
+//
+//  Created by Dhiman Das on 07.08.25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatMateTests/AIChatMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatMateTests.swift
+//  AIChatMateTests
+//
+//  Created by Dhiman Das on 07.08.25.
+//
+
+import Testing
+@testable import AIChatMate
+
+struct AIChatMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
